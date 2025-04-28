80c026024f45e9374169982dd6648ce2c112550b
horomalimentos <horom.alimentos@gmail.com>
2025-04-28 01:19:45 -0600

    Update Articulo.js

--- models/Articulo.js
+++ models/Articulo.js
@@ -1,1 +1,24 @@
+// models/Articulo.js
+const mongoose = require('mongoose');
+
+const articuloSchema = new mongoose.Schema({
+  nombre: { 
+    type: String, 
+    required: true 
+  },
+  proveedor: { 
+    type: String, 
+    required: true 
+  },
+  area: { 
+    type: String, 
+    required: true 
+  },
+  precio: { 
+    type: Number, 
+    required: true 
+  }
+});
+
+module.exports = mongoose.model('Articulo', articuloSchema);
 
